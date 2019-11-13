b54e28eebab52c07174b254038814f160b9a6a8f
Retno <retnoscwulandari@gmail.com>
2019-11-13 13:40:47 +0000

    first commit

---
+++ src/Counter.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  // Declares a new state variable, which we will call "count"
+  // Declares a "setCount function" that allows you to modify the count value
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <label>
+        The counter is at :
+        <input value={count} onChange={e => setCount(e.target.value)} />
+      </label>
+      <br />
+      <button onClick={() => setCount(count + 1)}> +1</button>
+      <button onClick={() => setCount(count - 1)}> -1</button>
+    </div>
+  );
+};
+
+export default Counter;
